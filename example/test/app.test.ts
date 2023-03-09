1977cf1b6603800ce0521eadbd4e250e175454dc
Stephen Mathieson <me@stephenmathieson.com>
2023-03-09 13:11:13 -0500

    more tests

--- example/test/app.test.ts
+++ example/test/app.test.ts
@@ -9,7 +9,26 @@ describe("App", () => {
     it("has links in the header", () => {
       cy.visit("http://localhost:5012");
       cy.get("header").should("exist");
-      cy.get('header a[href="/violations"]').should("exist").click();
+      cy.get('header a[href="/violations"]').should("exist");
+    });
+  });
+
+  describe("violations page", () => {
+    it("has a title", () => {
+      cy.visit("http://localhost:5012/violations")
+        .title()
+        .should("eq", "Violations | App");
+    });
+
+    it("has links in the header", () => {
+      cy.visit("http://localhost:5012/violations");
+      cy.get("header").should("exist");
+      cy.get('header a[href="/"]').should("exist");
+    });
+
+    it("has images of cats", () => {
+      cy.visit("http://localhost:5012/violations");
+      cy.get("img").should("have.length", 10);
     });
   });
 });
